1b2d096ccee7d28394b23a4e333f22223f5f4614
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-07-25 00:52:43 +0700

    Update masuk.php

--- api/masuk.php
+++ api/masuk.php
@@ -32,7 +32,7 @@
       <div class="max-w-md w-full px-6 py-8 bg-white rounded-lg lg:shadow-md">
         <h2 class="text-2xl font-semibold text-center text-gray-800 mb-5">Masuk</h2>
 
-        <form action="../controllers/AuthController.php?action=login" method="post">
+        <form action="./AuthController.php?action=login" method="post">
 
           <div class="mb-4">
             <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email</label>
