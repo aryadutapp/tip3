2b83da963ccf26ffbc9f71f0f7a929183c3b2660
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-07-27 14:28:53 +0700

    Update and rename UserModel.php to models.php

---
+++ api/models.php
@@ -0,0 +1,53 @@
+<?php
+require_once 'database.php';
+
+class User {
+    public $id;
+    public $email;
+    public $password;
+    public $status;
+
+    public function __construct($email, $password, $status) {
+        $this->email = $email;
+        $this->password = $password;
+        $this->status = $status;
+    }
+
+    // Create a new user in the database
+    public function createUser() {
+        global $db;
+        $hashedPassword = $this->password; // Replace this with the actual hashing logic
+
+        $query = "INSERT INTO data_user (email, password, status) VALUES (?, ?, ?)";
+        $stmt = $db->prepare($query);
+        $stmt->execute([$this->email, $hashedPassword, $this->status]);
+        return true; // Return true to indicate successful creation
+    }
+
+    // Read user data from the database by email
+    public static function getUserByEmail($email) {
+        global $db;
+        $query = "SELECT * FROM data_user WHERE email = ?";
+        $stmt = $db->prepare($query);
+        $stmt->execute([$email]);
+        return $stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+    // Update user data in the database
+    public function updateUser() {
+        global $db;
+        $query = "UPDATE data_user SET email = ?, password = ?, status = ? WHERE id = ?";
+        $stmt = $db->prepare($query);
+        $stmt->execute([$this->email, $this->password, $this->status, $this->id]);
+        return true; // Return true to indicate successful update
+    }
+
+    // Delete user from the database by ID
+    public static function deleteUserById($id) {
+        global $db;
+        $query = "DELETE FROM data_user WHERE id = ?";
+        $stmt = $db->prepare($query);
+        $stmt->execute([$id]);
+        return true; // Return true to indicate successful deletion
+    }
+}
