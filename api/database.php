e3e87dbac0173336d9fd6aecac254acc72fe21a2
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-07-27 14:49:47 +0700

    Update database.php

--- api/database.php
+++ api/database.php
@@ -1,19 +1,16 @@
-
 <?php
 class Database {
-$dbhost = "ep-odd-paper-540852-pooler.us-east-1.postgres.vercel-storage.com";
-$dbname = "verceldb";
-$dbuser = "default";
-$dbpassword = "xXk9cTjer8uA";
-$dbopt = "endpoint=ep-odd-paper-540852";
+    private static $dbhost = "ep-odd-paper-540852-pooler.us-east-1.postgres.vercel-storage.com";
+    private static $dbname = "verceldb";
+    private static $dbuser = "default";
+    private static $dbpassword = "xXk9cTjer8uA";
+    private static $dbopt = "endpoint=ep-odd-paper-540852";
+    private static $connection = null;
 
     public static function getConnection() {
         if (self::$connection === null) {
-$connectionString = "host=$dbhost dbname=$dbname user=$dbuser password=$dbpassword options=$dbopt";
+            $connectionString = "host=" . self::$dbhost . " dbname=" . self::$dbname . " user=" . self::$user . " password=" . self::$password . " options=" . self::$dbopt;
             self::$connection = pg_connect($connectionString);
-
-       
-
 
             if (!self::$connection) {
                 // Failed to connect to the database
@@ -24,6 +21,3 @@ $connectionString = "host=$dbhost dbname=$dbname user=$dbuser password=$dbpasswo
         return self::$connection;
     }
 }
-
-
-?>
