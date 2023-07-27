95a868a70602b5266bd476fdb8a01214abf7d4b4
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-07-27 14:35:50 +0700

    Update database.php

--- api/database.php
+++ api/database.php
@@ -1,40 +1,28 @@
+
 <?php
-// Replace these with your actual database credentials
-$dbhost = "ep-odd-paper-540852-pooler.us-east-1.postgres.vercel-storage.com";
-$dbname = "verceldb";
-$dbuser = "default";
-$dbpassword = "xXk9cTjer8uA";
-$dbopt = "endpoint=ep-odd-paper-540852";
-
 class Database {
-    private $host;
-    private $dbname;
-    private $user;
-    private $password;
-    private $options;
-    private $connection;
+    private static $host = "ep-odd-paper-540852-pooler.us-east-1.postgres.vercel-storage.com";
+    private static $dbname = 'verceldb';
+    private static $user = 'default';
+    private static $password = 'xXk9cTjer8uA';
+    private static $options = "endpoint=ep-odd-paper-540852";
+    private static $connection = null;
 
-    public function __construct() {
-        global $dbhost, $dbname, $dbuser, $dbpassword, $dbopt;
-        $this->host = $dbhost;
-        $this->dbname = $dbname;
-        $this->user = $dbuser;
-        $this->password = $dbpassword;
-        $this->options = $dbopt;
-
-        $dsn = "pgsql:host=$this->host;dbname=$this->dbname;$this->options";
-
-        try {
-            $this->connection = new PDO($dsn, $this->user, $this->password);
-            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            // Handle connection error if needed
-            echo "Connection to the database failed: " . $e->getMessage();
-            die();
+    public static function getConnection() {
+        if (self::$connection === null) {
+            try {
+                $dsn = "pgsql:host=" . self::$host . ";dbname=" . self::$dbname . ";" . self::$options;
+                self::$connection = new PDO($dsn, self::$user, self::$password);
+                self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                // Handle connection error if needed
+                die("Connection to the database failed: " . $e->getMessage());
+            }
         }
-    }
 
-    public function getConnection() {
-        return $this->connection;
+        return self::$connection;
     }
 }
+
+
+?>
