c3cf8a367c9fa4aac51f51a8177a536f0a0406ad
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-07-28 10:11:33 +0700

    regis script

--- api/daftar.php
+++ api/daftar.php
@@ -26,7 +26,7 @@
       <div class="max-w-md w-full px-6 py-8 bg-white rounded-lg shadow-md">
         <h2 class="text-2xl font-semibold text-center text-gray-800 mb-5">Daftar</h2>
 
-        <form action="./AuthController.php?action=register" method="post">
+        <form action="./AuthController.php?action=register" method="post" onsubmit="return validateForm()" >
 
           <div class="mb-4">
             <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email</label>
@@ -83,6 +83,8 @@
       </div>
     </div>
   </section>
+  <script src="../assets/js/regis.js"></script>
+
 </main>
 
 <footer class="border-t border-gray-200 bg-gray-100">
@@ -113,6 +115,7 @@
     </div>
   </div>
 </footer>
+
 
 </body>
 </html>
