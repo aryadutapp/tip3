4a7dd1cc94dcb4a76b5add9438a1a4464764b97b
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-07-29 15:33:05 +0700

    Update controllers.php

--- api/controllers.php
+++ api/controllers.php
@@ -24,18 +24,31 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             exit();
         }
     } elseif ($form_action === "register") {
-        // Assuming your form has input fields with names "email", "password", and "status"
-        $reg_email = $_POST["email"];
-        $reg_password = $_POST["password"];
-        $reg_status = $_POST["status"];
+        
+   // Assuming your form has input fields with names "email", "password", and "status"
+    $reg_email = $_POST["email"];
+    $reg_password = $_POST["password"];
+    $reg_status = $_POST["status"];
 
+    // Check if the email is already registered
+    $existingUser = User::getUserByEmail($reg_email);
+    if ($existingUser !== -1) {
+        // Email is already registered, display an error message
+        $message = "Email is already registered.";
+                    $errorMessage = "Email sudah terdaftar";
+            $encodedErrorMessage = urlencode($errorMessage);
+            header("Location: daftar.php?error=$encodedErrorMessage");
+            exit();
+    } else {
+        // Email is not registered, proceed with user registration
         $newUser = new User($reg_email, $reg_password, $reg_status);
-
         if ($newUser->createUser()) {
             $message = "Registration successful!";
         } else {
             $message = "Failed to register user.";
         }
+    }
+        
     } else {
         // If form_action is not "login" or "register", you can handle the specific case here
         $message = "Method Post But Invalid form_action: $form_action";
