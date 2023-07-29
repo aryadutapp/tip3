0aafd0fd9cb2a7868f5b45c5fc771f24740bf256
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-07-29 17:26:00 +0700

    Update logout.php

--- api/logout.php
+++ api/logout.php
@@ -1,24 +1,28 @@
 <?php
 require_once 'models.php';
 
-// Assuming you have the user's cookie value from the current session
-// Replace this with the actual method to get the user's cookie value
-$cookieValue = $_COOKIE['titip_user'];
+// Check if the "titip_user" cookie is set
+if (isset($_COOKIE['titip_user'])) {
+    // Get the user's cookie value from the current session
+    $cookieValue = $_COOKIE['titip_user'];
 
-// Get the user's email based on their session
-$userEmail = User::getUserEmailBySession($cookieValue);
+    // Get the user's email based on their session
+    $userEmail = User::getUserEmailBySession($cookieValue);
 
-// Now you have the user's email, and you can use it for further operations
-// ...
+    // If a valid user email is retrieved, perform the logout actions
+    if ($userEmail) {
+        // Perform the logout actions
+        User::deleteSessionByEmail($userEmail);
 
-// Perform the logout actions
-User::deleteSessionByEmail($userEmail);
+        // Clear the cookie by setting it to an empty value and expiring it (time in the past)
+        setcookie("titip_user", "", time() - 3600, '/');
 
-// Clear the cookie by setting it to an empty value and expiring it (time in the past)
-setcookie("titip_user", "", time() - 3600, '/');
+        // Redirect to index.html after logout
+        header("Location: ../");
+        exit();
+    }
+}
 
-// Redirect to index.html after logout
+// If there is no "titip_user" cookie or the user email is not found, directly redirect to index.html
 header("Location: ../");
 exit();
-
-?>
