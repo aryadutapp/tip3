849c068bc95e276a4d5c9c849f6b460918dd7bb2
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-07-29 17:17:51 +0700

    Create logout.php

---
+++ api/logout.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'models.php';
+
+// Assuming you have the user's cookie value from the current session
+// Replace this with the actual method to get the user's cookie value
+$cookieValue = $_COOKIE['titip_user'];
+
+// Get the user's email based on their session
+$userEmail = User::getUserEmailBySession($cookieValue);
+
+// Now you have the user's email, and you can use it for further operations
+// ...
+
+// Perform the logout actions
+User::deleteSessionByEmail($userEmail);
+
+// Clear the cookie by setting it to an empty value and expiring it (time in the past)
+setcookie("titip_user", "", time() - 3600, '/');
+
+// Redirect to index.html after logout
+header("Location: index.html");
+exit();
+
+?>
