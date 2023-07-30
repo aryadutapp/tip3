db3bb2bca6b104b8efae6171c5fef6c97426e347
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-07-30 17:22:30 +0700

    Update masuk.php

--- api/masuk.php
+++ api/masuk.php
@@ -87,7 +87,13 @@ $emailFromHeader = isset($_GET["email"]) ? urldecode($_GET["email"]) : '';
         $errorMessage = urldecode($_GET["error"]);
         echo "<p class='text-md text-red-500 my-6 text-center'>$errorMessage</p>";
     }
+    if (isset($_GET["success"])) {
+        $successMessage = urldecode($_GET["error"]);
+        echo "<p class='text-md text-green-500 my-6 text-center'>$successMessage</p>";
+    }
     ?>
+    
+        
 
           <div class="flex items-center justify-center">
             <button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Masuk</button>
