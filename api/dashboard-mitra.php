982c57fb2ccbb92fcdf79979661a3346ddf9cce3
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-07-31 14:55:04 +0700

    input hiden pesanan masuk

--- api/dashboard-mitra.php
+++ api/dashboard-mitra.php
@@ -157,10 +157,7 @@ if (!$user || $user->status !== "mitra") {
                                                             <option value="L">L (Large)</option>
                                                         </select>
                                                     </div>
-                                                    <div>
-                                                        <label for="id-barang" class="block mb-2 text-sm font-medium text-gray-900">ID Barang (Auto Generated)</label>
-                                                        <input type="text" name="id-barang" id="id-barang" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" readonly="" required="">
-                                                    </div>
+                                                    <input type="hidden" name="form_action" value="pesanan-masuk">
                                                     <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Masukkan Pesanan</button>
                                                 </form>
                                             </div>
