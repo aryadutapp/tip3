dd67534104e23f6a9f9b71bb133b695111bc7910
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-08-01 18:45:00 +0700

    limit fetch

--- api/fetch_store.php
+++ api/fetch_store.php
@@ -28,7 +28,7 @@ function fetchReservations() {
     $query = "SELECT *
               FROM data_reservasi
               WHERE store_id = $1
-              LIMIT 10 OFFSET 10";
+              LIMIT 10";
     $result = pg_query_params($db, $query, [$user->user_id]);
 
     if (!$result) {
