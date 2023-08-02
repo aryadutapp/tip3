6744736798ced3fe799cd3e8fbd6dbaa31449bb4
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-08-02 21:45:04 +0700

    cari nama

--- api/cari-nama.php
+++ api/cari-nama.php
@@ -1,14 +1,54 @@
 <?php
-// Simulate database or data source
-$mockData = ['John Doe', 'Jane Smith', 'Robert Johnson', 'Alice Johnson', 'David Brown'];
+require_once 'models.php';
 
-$query = isset($_GET['query']) ? $_GET['query'] : '';
+// Function to fetch customer names based on the search query
+function fetchCustomerNames($store_id, $query) {
+    $cookieValue = isset($_COOKIE['titip_user']) ? $_COOKIE['titip_user'] : null;
+    $userEmail = User::getUserEmailBySession($cookieValue);
 
-$filteredResults = array_filter($mockData, function ($name) use ($query) {
-    return stripos($name, $query) !== false;
-});
+    if (!$userEmail) {
+        // Redirect to "masuk.php" with an error message if session not found
+        $errorMessage = "Sesi telah berakhir. Silakan masuk kembali.";
+        $encodedErrorMessage = urlencode($errorMessage);
+        header("Location: masuk.php?error=$encodedErrorMessage");
+        exit();
+    }
 
-// Return JSON response
-header('Content-Type: application/json');
-echo json_encode($filteredResults);
+    // Fetch reservations for the user based on their email
+    $user = User::getUserByEmail($userEmail);
+
+    if (!$user) {
+        // Handle the case when user not found (e.g., return an error code or show a message)
+        die("User not found.");
+    }
+
+    $db = Database::getConnection();
+    $query = "SELECT CUST_NAME
+              FROM data_reservasi
+              WHERE store_id = $1 AND CUST_NAME ILIKE $2
+              LIMIT 10";
+    $result = pg_query_params($db, $query, [$store_id, "%$query%"]);
+
+    if (!$result) {
+        // Handle the error (e.g., log or show an error message)
+        die("Error executing query: " . pg_last_error($db));
+    }
+
+    // Fetch all rows from the result set as an array of associative arrays
+    $rows = pg_fetch_all($result);
+
+    // Extract customer names from the result rows
+    $customerNames = array_column($rows, 'cust_name');
+
+    // Return the data as JSON
+    header('Content-Type: application/json');
+    echo json_encode($customerNames);
+}
+
+// Get store_id and query from the request
+$store_id = isset($_POST['store_id']) ? $_POST['store_id'] : null;
+$query = isset($_POST['query']) ? $_POST['query'] : '';
+
+// Call the function to fetch customer names
+fetchCustomerNames($store_id, $query);
 ?>
