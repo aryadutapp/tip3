2ead518f756462ff6e66a37984936c8dab6b5c55
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-08-03 18:26:10 +0700

    mitra2 size masuk

--- api/mitra2.php
+++ api/mitra2.php
@@ -240,6 +240,16 @@ if (!$user || $user->status !== "mitra") {
             <!-- Dropdown options will be dynamically populated -->
         </select>
     </div>
+
+    <div>
+                                                        <label for="size" class="block mb-2 text-sm font-medium text-gray-900">Ukuran</label>
+                                                        <select name="size" id="size" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required="">
+                                                            <option value="" disabled="" selected="">Pilih Ukuran</option>
+                                                            <option value="S">S (Small)</option>
+                                                            <option value="L">L (Large)</option>
+                                                        </select>
+                                                    </div>
+
     <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Masukkan Pesanan</button>
 </form>
 
