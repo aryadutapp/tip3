88d34c6535afab24479e410c9087c4dad60f1f02
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-09-09 01:29:54 +0700

    logout

--- api/dashboard-konsumen.php
+++ api/dashboard-konsumen.php
@@ -91,7 +91,7 @@ if (!$user || $user->status !== "konsumen") {
                             </a>
                         </li>
                         <li>
-                            <a href="./index" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
+                            <a href="./logout.php" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
                                 <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"></path>
                                 </svg>
