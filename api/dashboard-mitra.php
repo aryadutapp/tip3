81308c8c69c110e308ca1705eedc464440decc70
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-09-09 08:06:56 +0700

    pesanan masuk eror

--- api/dashboard-mitra.php
+++ api/dashboard-mitra.php
@@ -188,16 +188,21 @@ if (!$user || $user->status !== "mitra") {
                                                         })
                                                         .then(response => response.json())
                                                         .then(data => {
-                                                            if (data.status_pesanan_masuk == 'success') {
+                                                            if (data.status_pesanan_masuk === 'success') {
                                                                 // Display success message as a popup
                                                                 alert('Pesanan berhasil masuk!');
                                                                 // You can also redirect to another page if needed
                                                                 window.location.href = 'dashboard-mitra.php';
                                                             } else {
                                                                 // Display error message as a popup
-                                                                alert('Gagal memasukkan pesanan. Silakan coba lagi.' + data.status_pesanan_masuk);
+                                                                alert('Gagal memasukkan pesanan. Silakan coba lagi.');
                                                             }
                                                         })
+                                                        .catch(error => {
+                                                            console.error('Error:', error);
+                                                            // Handle any network or server error here
+                                                            alert('Terjadi kesalahan. Silakan coba lagi nanti.'+ data.status_pesanan_masuk);
+                                                        });
                                                     }
                                                 </script>
 
