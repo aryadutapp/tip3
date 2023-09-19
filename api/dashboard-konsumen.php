7704ad5af9253fcd4f2325c7bd4faa6648d8bb18
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-09-19 20:47:41 +0700

    konsumen

--- api/dashboard-konsumen.php
+++ api/dashboard-konsumen.php
@@ -689,6 +689,9 @@ if (!$user || $user->status !== "konsumen") {
                             </select>
                         </div>
                         <input type="hidden" name="form_action" value="pesanan-masuk">
+
+                        <input type="hidden" name="user_id_mitra" value="${data.user_id}">
+
                         
                         <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Masukkan Pesanan</button>
                     </form>
