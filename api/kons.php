821bee2ed7d991844fab4f51608ed6e320586025
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-09-30 11:46:49 +0700

    KONS

--- api/kons.php
+++ api/kons.php
@@ -629,7 +629,7 @@ if (!$user || $user->status !== "konsumen") {
                 <th scope="col" class="px-4 py-3">NAMA TOKO</th>
                <!-- <th scope="col" class="px-4 py-3">ALAMAT</th> -->
                <!-- <th scope="col" class="px-4 py-3">KELURAHAN</th> -->
-                <th scope="col" class="px-4 py-3">KOTA</th>
+                <th scope="col" class="px-4 py-3">ALAMAT</th>
                 <th scope="col" class="px-4 py-3">
                     <span class="sr-only">CETAK STRUK</span>
                 </th>
@@ -742,7 +742,7 @@ if (!$user || $user->status !== "konsumen") {
             modal.classList.remove(); // Remove all existing classes
             modal.classList.add('fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full justify-center items-center flex'); // Add your new class
         }
-        
+
         const backdrop = document.createElement('div');
         backdrop.setAttribute('modal-backdrop', '');
         backdrop.classList.add('bg-gray-900', 'bg-opacity-50', 'dark:bg-opacity-80', 'fixed', 'inset-0', 'z-40');
