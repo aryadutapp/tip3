649a42d83c947b3073feb71289699b6e245d3f50
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-09-30 12:03:03 +0700

    HIDDEN KONS

--- api/kons.php
+++ api/kons.php
@@ -740,7 +740,23 @@ if (!$user || $user->status !== "konsumen") {
         
         if (modal) {
             modal.classList.remove(); // Remove all existing classes
-            modal.classList.add('fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full justify-center items-center flex'); // Add your new class
+            modal.classList.add(
+            'fixed',
+            'top-0',
+            'left-0',
+            'right-0',
+            'z-50',
+            'w-full',
+            'p-4',
+            'overflow-x-hidden',
+            'overflow-y-auto',
+            'md:inset-0',
+            'h-[calc(100%-1rem)]',
+            'max-h-full',
+            'justify-center',
+            'items-center',
+            'flex'
+        );
         }
     }
 
