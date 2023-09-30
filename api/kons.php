72bfaeceb83c1436004596298f2d40fa8c0b6e1a
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-09-30 11:26:03 +0700

    kons close

--- api/kons.php
+++ api/kons.php
@@ -672,12 +672,14 @@ if (!$user || $user->status !== "konsumen") {
           <div class="relative w-full max-w-md max-h-full">
               <!-- Modal content -->
               <div class="relative bg-white rounded-lg shadow ">
-                  <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center" data-modal-hide="pesanan_masuk_konsumen_${userIDValue}">
+
+                  <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center" data-modal-hide="pesanan_masuk_konsumen">
                       <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                       </svg>
                       <span class="sr-only">Close modal</span>
                   </button>
+
                   <div class="px-6 py-6 lg:px-8">
                       <h3 class="mb-4 text-xl font-medium text-gray-900 ">Pesan di ${namaTokoValue}</h3>
                       <form class="space-y-6" action="https://www.titipin.com/api/controllers.php?action=register" onsubmit="return handleSubmit(event, this)" method="post">
@@ -714,6 +716,11 @@ if (!$user || $user->status !== "konsumen") {
             button.addEventListener('click', () => openPesananMasukModal(data, userIDValue)); // Pass userIDValue as an argument
         });
 
+        const buttonscl = row.querySelectorAll('[data-modal-hide="pesanan_masuk_konsumen"]');
+        buttonscl.forEach(button => {
+            button.addEventListener('click', () => closePesananMasukModal(data, userIDValue)); // Pass userIDValue as an argument
+        });
+
         return row;
     }
 
@@ -730,6 +737,19 @@ if (!$user || $user->status !== "konsumen") {
         
         if (modal) {
             modal.classList.remove('hidden');
+        }
+    }
+
+    function closePesananMasukModal(data, userID) {
+        // Handle the modal opening logic here
+        // ...
+
+        // Assuming you want to open the modal when the button is clicked
+        const modalId = `pesanan_masuk_konsumen_${userID}`;
+        const modal = document.getElementById(modalId);
+        
+        if (modal) {
+            modal.classList.add('hidden');
         }
     }
 
