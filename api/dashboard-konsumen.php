0a991fce96ed9b9a29ce3e7e5bd653cfafe53fbb
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-10-02 20:57:52 +0700

    jarak menu

--- api/dashboard-konsumen.php
+++ api/dashboard-konsumen.php
@@ -44,7 +44,7 @@ if (!$user || $user->status !== "konsumen") {
     </header>
     <main id="menu-utama" class="mt-24">
       <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform sm:translate-x-0 -translate-x-full" aria-label="Sidebar" aria-hidden="true">
-        <div class="mt-20 h-full px-3 py-4 overflow-y-auto bg-gray-50">
+        <div class="mt-16 h-full px-3 py-4 overflow-y-auto bg-gray-50">
           <ul class="space-y-2 font-medium">
             <li><a href="#" class="flex items-center p-2 text-gray-900 rounded-lg bg-gray-300 group"><svg class="w-5 h-5 text-gray-500 transition duration-75text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
                   <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"></path>
