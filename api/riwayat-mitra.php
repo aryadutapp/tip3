f6f4e8e5ebc1ead4e1887bc4d977c5770cae2df8
aryadutapp <89324353+aryadutapp@users.noreply.github.com>
2023-10-04 21:59:38 +0700

    pesanan keluar

--- api/riwayat-mitra.php
+++ api/riwayat-mitra.php
@@ -345,6 +345,121 @@ if (!$user || $user->status !== "mitra") {
 
 
 
+                    <section id="riwayat-pesanan-keluar" class="p-4 sm:ml-64">
+            <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg">
+            <div class="mx-auto">
+            <div class="rounded-t-lg bg-gray-100 p-6">
+            <h2 class="text-2xl font-bold mb-4 text-black">Riwayat Pesanan Keluar</h2>
+            <div class="mx-auto max-w-screen-xl px-4 lg:px-12">
+            <!-- Start coding here -->
+            <div class="bg-white relative shadow-md sm:rounded-lg overflow-hidden">
+            <!-- // buat search bar-->
+            <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4 hidden">
+            <div class="w-full md:w-1/2">
+                <form class="flex items-center">
+                    <label for="simple-search" class="sr-only">Search</label>
+                    <div class="relative w-full">
+                        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
+                        </svg>
+                        </div>
+                        <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2" placeholder="Search" required="">
+                    </div>
+                </form>
+            </div>
+            </div>
+            <div class="overflow-x-auto">
+            <table class="w-full text-sm text-left text-gray-500">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                    <tr>
+                        <th scope="col" class="px-4 py-3">INDEX</th>
+                        <th scope="col" class="px-4 py-3">RESERVATION_ID</th>
+                        <th scope="col" class="px-4 py-3">CUST_EMAIL</th>
+                        <th scope="col" class="px-4 py-3">CUST_NAME</th>
+                        <th scope="col" class="px-4 py-3">PICKUP NUMBER</th>
+                        <th scope="col" class="px-4 py-3">BOOK_TIME</th>
+                        <th scope="col" class="px-4 py-3">START_TIME</th>
+                        <th scope="col" class="px-4 py-3">END_TIME</th>
+                        <th scope="col" class="px-4 py-3">SIZE</th>
+                        <th scope="col" class="px-4 py-3">RESERVATION_STATUS</th>
+                    </tr>
+                </thead>
+                <tbody id="pesanan-keluar-table-body">
+                    <!-- Table rows will be dynamically added here -->
+                </tbody>
+            </table>
+            </div>
+            <script>
+            // Function to create table rows with data
+            function createTableRow(data) {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">${data.index}</td>
+                <td class="px-4 py-3">${data.reservation_id}</td>
+                <td class="px-4 py-3">${data.cust_email}</td>
+                <td class="px-4 py-3">${data.cust_name}</td>
+                <td class="px-4 py-3">${data.pickup_number}</td>
+                <td class="px-4 py-3">${data.book_time}</td>
+                <td class="px-4 py-3">${data.start_time}</td>
+                <td class="px-4 py-3">${data.end_time || 'N/A'}</td>
+                <td class="px-4 py-3">${data.size}</td>
+                <td class="px-4 py-3">${data.reservation_status}</td>
+                <td class="px-4 py-3 flex items-center justify-end">
+                    <button data-dropdown-toggle="actions-dropdown" class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none" type="button">
+                        <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
+                        </svg>
+                    </button>
+                    <div id="actions-dropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow" data-popper-placement="bottom" style="position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate3d(1077px, 628.5px, 0px);">
+                        <ul class="py-1 text-sm text-gray-700" aria-labelledby="actions-dropdown">
+                            <li>
+                                <a href="#" class="block py-2 px-4 hover:bg-gray-100">Show</a>
+                            </li>
+                            <li>
+                                <a href="#" class="block py-2 px-4 hover:bg-gray-100">Edit</a>
+                            </li>
+                        </ul>
+                        <div class="py-1">
+                            <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Delete</a>
+                        </div>
+                    </div>
+                </td>
+            `;
+            return row;
+            }
+            
+            // Function to fetch data from the server and populate the table
+            function AmbilDataPesananKeluar() {
+            fetch("fetch_store_lengkap.php")
+            .then(response => response.json())
+            .then(data => {
+                const tableBody = document.getElementById("pesanan-keluar-table-body");
+                data.forEach(rowData => {
+                    if (rowData.reservation_status == "PESANAN KELUAR") {
+                        const newRow = createTableRow(rowData);
+                        tableBody.appendChild(newRow);
+                    }
+                });
+            })
+            .catch(error => console.error("Error:", error));
+            }
+            
+            
+            // Call the function to populate the table on page load
+            AmbilDataPesananKeluar();
+            </script>
+
+
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </section>
+
+
+
 
         </main>
         <script src="https://flowbite.com/docs/flowbite.min.js"></script>
